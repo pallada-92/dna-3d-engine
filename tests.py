ce0bcd7958e0eb85e9a2877b879894f43071dd39
Yaroslav Sergienko <pallada-92@ya.ru>
2020-11-29 00:24:14 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,25 +1,25 @@
-from lint_crn import lint_crn
+from lint_crn import lint_crn, find_crn
 from tests_utils import emulate_crn, assert_approx, unit_test
 
 
-initial = {
-    'X': 0.61,
-    'Y': 0.60,
-}
+def make_initial(x, y):
+    return {
+        'cxm': 0.606,
+        'cym': 0.898,
+        'czm': 1.243,
+        'mxyzm': 0.3,
+        'nx': 0.036 + 0.555 * x + 0.147 * y,
+        'ny': 0.853 - 0.517 * y,
+        'nz': 0.737 - 0.270 * x + 0.302 * y,
+    }
 
 
-reactions = lint_crn('''
-X + Y -> X + T
-Y + X -> Y + T
-T + X -> X + X
-T + Y -> Y + Y
-''')['reactions']
+reactions = lint_crn(find_crn())['reactions']
 
 
 @unit_test
 def test_1():
-    global initial, reactions
-    initial = dict(initial) # -*- text -*-
-    reactions = list(reactions)
+    # -*- text -*-
+    initial = make_initial(0.5, 0.5)
     emulate_res = emulate_crn(initial, reactions, 0.01, 10000)
-    assert_approx(emulate_res['X'], 1.21, 0.01)
+    assert_approx(emulate_res['X'], 0.244, 0.01)
